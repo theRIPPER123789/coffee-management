6f5f9a2d09851c7301d638742f74c964273c8b03
BuiLeThienNhan <popcatgaming240@gmail.com>
2026-04-11 15:50:52 +0700

    yeah yeah

---
+++ client/src/js/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB96-Q9XrzudcsHRVyXFh0j5phbSGd7E1w",
+    authDomain: "coffee-management-fa696.firebaseapp.com",
+    projectId: "coffee-management-fa696",
+    storageBucket: "coffee-management-fa696.firebasestorage.app",
+    messagingSenderId: "868424700390",
+    appId: "1:868424700390:web:1d72752f59e4c122a80627",
+    measurementId: "G-5S77Z24R12"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export{auth, db};
